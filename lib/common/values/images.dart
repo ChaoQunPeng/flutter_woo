0841ccced031f079b452f3d78794494ca3a19382
PengChaoQun <1152684231@qq.com>
2024-06-02 19:24:01 +0800

    ✨ feat: 引入图标

--- lib/common/values/images.dart
+++ lib/common/values/images.dart
@@ -1,3 +1,22 @@
+/*
+ * @Author: PengChaoQun 1152684231@qq.com
+ * @Date: 2024-06-01 16:24:42
+ * @LastEditors: PengChaoQun 1152684231@qq.com
+ * @LastEditTime: 2024-06-02 19:07:00
+ * @FilePath: /flutter_woo_commerce_getx_learn/lib/common/values/images.dart
+ * @Description: 
+ */
 /// 图片 assets
 class AssetsImages {
+  static const defaultPng = 'assets/images/default.png';
+  static const orderConfirmedPng = 'assets/images/order-confirmed.png';
+  static const pCashPng = 'assets/images/p-cash.png';
+  static const pMastercardPng = 'assets/images/p-mastercard.png';
+  static const pPaypalPng = 'assets/images/p-paypal.png';
+  static const pVisaPng = 'assets/images/p-visa.png';
+  static const profileBackgroundPng = 'assets/images/profile-background.png';
+  static const welcomePng = 'assets/images/welcome.png';
+  static const welcome_1Png = 'assets/images/welcome_1.png';
+  static const welcome_2Png = 'assets/images/welcome_2.png';
+  static const welcome_3Png = 'assets/images/welcome_3.png';
 }

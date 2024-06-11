83e383229212f1eeb1abe2ad72ee60917124fb50
PengChaoQun <1152684231@qq.com>
2024-06-11 15:34:48 +0800

    ✨ feat: 商品标题栏

--- lib/common/widgets/index.dart
+++ lib/common/widgets/index.dart
@@ -2,7 +2,7 @@
  * @Author: PengChaoQun 1152684231@qq.com
  * @Date: 2024-06-01 16:24:42
  * @LastEditors: PengChaoQun 1152684231@qq.com
- * @LastEditTime: 2024-06-08 09:54:32
+ * @LastEditTime: 2024-06-11 15:32:15
  * @FilePath: /flutter_woo_commerce_getx_learn/lib/common/widgets/index.dart
  * @Description: 
  */
@@ -15,3 +15,4 @@ export './button.dart';
 export './input.dart';
 export './text_form.dart';
 export './navigation.dart';
+export './icon_text.dart';

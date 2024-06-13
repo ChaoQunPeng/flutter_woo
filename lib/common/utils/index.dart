f452bcd510128f81b157654bd3bfac22feed5987
PengChaoQun <1152684231@qq.com>
2024-06-13 18:07:12 +0800

    ✨ feat: 增加设备动态权限查询

--- lib/common/utils/index.dart
+++ lib/common/utils/index.dart
@@ -2,7 +2,7 @@
  * @Author: PengChaoQun 1152684231@qq.com
  * @Date: 2024-06-01 16:24:42
  * @LastEditors: PengChaoQun 1152684231@qq.com
- * @LastEditTime: 2024-06-12 21:02:35
+ * @LastEditTime: 2024-06-13 18:04:59
  * @FilePath: /flutter_woo_commerce_getx_learn/lib/common/utils/index.dart
  * @Description: 
  */
@@ -15,3 +15,4 @@ export 'encrypt.dart';
 export 'picker.dart';
 export 'bottom_sheet.dart';
 export 'convert.dart';
+export 'privilege.dart';
